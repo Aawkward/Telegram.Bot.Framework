3e2897c46d8f0279abf37c3fe35dc1d4c938d4ce
asiryan <dmc5mod@yandex.ru>
2023-01-16 10:42:38 +0300

    Update packages

--- src/Extensions/MiddlewareExtensions.cs
+++ src/Extensions/MiddlewareExtensions.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Telegram.Bot;
 using Telegram.Bot.Framework;
 using Telegram.Bot.Framework.Abstractions;
 
